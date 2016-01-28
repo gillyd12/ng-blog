9764051e109ed6757c5c0fe366283d9505a5875e
Bryan Gill <bryan@shipitweekly.com>
2016-01-27 18:44:47 -0600

    Adding temporary jspm registry export file for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,35 @@ module.exports = function (grunt) {
                     src: ['./src/config/config.js'],
                     dest: './src/js/modules/'
                 }]
+            },
+            ng_blog_dev: {
+                options: {
+                    patterns: [{
+                        json: grunt.file.readJSON('./src/config/environments/development.json')
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['./src/config/ng-blog.js'],
+                    dest: './'
+                }]
+            },
+            ng_blog_prod: {
+                options: {
+                    patterns: [{
+                        json: grunt.file.readJSON('./src/config/environments/production.json')
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['./src/config/ng-blog.js'],
+                    dest: './'
+                }]
             }
+
+
 
         }
 
@@ -43,11 +71,13 @@ module.exports = function (grunt) {
 
     // configure development environment
     grunt.registerTask('development', [
-        'replace:development'
+        'replace:development',
+        'replace:ng_blog_dev'
     ]);
 
     grunt.registerTask('production', [
-        'replace:production'
+        'replace:production',
+        'replace:ng_blog_prod'
     ]);
 
 };
---
+++ ng-blog.js
@@ -0,0 +1,1 @@
+module.exports = require('github:gillyd12/ng-blog@feature-blog-data-framework/src/js/bootstrap');
