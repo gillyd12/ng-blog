fe8e0a22c075c021fec636aff95829d6b4fc6e61
Bryan Gill <bryan@shipitweekly.com>
2016-02-04 16:12:48 -0600

    Removed stand alone config file and added to module

--- src/js/modules/application.js
+++ src/js/modules/application.js
@@ -2,9 +2,7 @@
  * Created by bryangill on 10/24/15.
  */
 import SummaryModule from '../summary/summary.m.js'
-import ConfigurationModule from './config'
 
 export default angular.module('ngBlog.module.application', [
     SummaryModule.name,
-    ConfigurationModule.name
 ]);
--- src/js/summary/summary.m.js
+++ src/js/summary/summary.m.js
@@ -5,9 +5,14 @@ import directive from './summary.d.js';
 import SummaryController from './summary.c.js';
 import summaryService from './summary.s.js';
 
-export default angular.module('ngBlog.summary.module', ['ngSanitize'])
+export default angular.module('app.summary', ['ngSanitize'])
     .directive('ngbSummary', directive)
     .controller('SummaryController', SummaryController)
-    .service('summaryService', summaryService);
+    .service('summaryService', summaryService)
+    .constant('configuration', {
+        environment: 'development',
+        api_port: '1337',
+        api_url: 'http://localhost'
+    });
 
 
