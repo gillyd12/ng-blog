4ba49e6eafd071b44a3104de09e9cd661178c403
Bryan Gill <bryan@shipitweekly.com>
2016-02-04 16:44:34 -0600

    removed app.js and added config to module

--- src/js/summary/summary.m.js
+++ src/js/summary/summary.m.js
@@ -5,7 +5,7 @@ import directive from './summary.d.js';
 import SummaryController from './summary.c.js';
 import summaryService from './summary.s.js';
 
-export default angular.module('app.summary', ['ngSanitize'])
+export default angular.module('ngBlog.summary', ['ngSanitize'])
     .directive('ngbSummary', directive)
     .controller('SummaryController', SummaryController)
     .service('summaryService', summaryService)
@@ -13,6 +13,13 @@ export default angular.module('app.summary', ['ngSanitize'])
         environment: 'development',
         api_port: '1337',
         api_url: 'http://localhost'
-    });
+    })
+    .config(['$locationProvider', ($locationProvider) => {
+        $locationProvider.html5Mode(true);
+    }])
+    .config(['$compileProvider', ($compileProvider) => {
+        $compileProvider.debugInfoEnabled(false);
+    }]);
+
 
 
--- src/js/test.js
+++ src/js/test.js
@@ -1,7 +1,7 @@
 /**
  * Created by Gillyd on 10/22/2015.
  */
-import mainModule from './app';
+import mainModule from './ngBlog';
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainModule.name], { strictDi: true });
