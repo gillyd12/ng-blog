bb5e74371fa99d15cd7a87c54b780292c2f7cd48
Bryan Gill <bryan@shipitweekly.com>
2016-04-12 21:05:09 -0500

    Remove defined height and width

--- src/js/media/media.d.js
+++ src/js/media/media.d.js
@@ -13,9 +13,7 @@ function media() {
         link: function (scope, element, attrs, mediaCtrl) {
             mediaCtrl.retrieveMedia(attrs.mediaId).then(function (response) {
                 element.css({
-                    'background-image': 'url(data:image/*;base64,' + response.data.guid + ')',
-                    'width': '1024px',
-                    'height': '768px'
+                    'background-image': 'url(data:image/*;base64,' + response.data.guid + ')'
                 });
             })
         }
